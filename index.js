754c5fd169c95399b0a2b2edb791c83544976116
jarpi <josep.arpi@gmail.com>
2016-10-30 20:24:58 +0100

    MVP finished Frontend, websockets
    Finished MVP, ready to test and improve.
    - Renders drawings based on input
    - Renders drawings based on websocket data
    - Sends draw updates through websockets

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ var express = require('express'),
     app = express(),
     nodemon = require('nodemon'),
     winston = require('winston'),
-    websocket = require('socket.io');
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
 
 app.use(express.static('src/www'));
-app.listen(process.env.port || 8080, function(){
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('data', function(msg){
+    console.log('received: ' + msg);
+    socket.broadcast.emit('data', msg);
+  });
+});
+
+http.listen(process.env.port || 8080, function(){
     winston.info('Application server running!');
 });
